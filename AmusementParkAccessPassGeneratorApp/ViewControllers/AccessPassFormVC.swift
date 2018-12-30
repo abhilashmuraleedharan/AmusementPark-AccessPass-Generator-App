1f158e44dbd11dd30c2d94e7898265e0b6272539
Abhilash Muraleedharan <amuraleedharan13@gmail.com>
2018-12-30 15:36:20 +0530

    Connects view items to view controller

--- AmusementParkAccessPassGeneratorApp/ViewControllers/AccessPassFormVC.swift
+++ AmusementParkAccessPassGeneratorApp/ViewControllers/AccessPassFormVC.swift
@@ -9,10 +9,64 @@
 import UIKit
 
 class AccessPassFormVC: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var subMenuView: UIStackView!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var dateOfVisitLabel: UILabel!
+    @IBOutlet weak var dateOfVisitTextField: UITextField!
+    @IBOutlet weak var projectNumberLabel: UILabel!
+    @IBOutlet weak var projectNumberTextField: UITextField!
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var streetAddressLabel: UILabel!
+    @IBOutlet weak var streetAddressTextField: UITextField!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var companyTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipcodeLabel: UILabel!
+    @IBOutlet weak var zipcodeTextField: UITextField!
+    @IBOutlet weak var generatePassButton: UIButton!
+    @IBOutlet weak var populateDataButton: UIButton!
+    
+    // MARK: - Stored Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - IB Actions
+    @IBAction func guestMenuOptionSelected(_ sender: Any) {
+    }
+    
+    @IBAction func employeeMenuOptionSelected(_ sender: Any) {
+    }
+    
+    @IBAction func managerMenuOptionSelected(_ sender: Any) {
+    }
+    
+    @IBAction func contractorMenuOptionSelected(_ sender: Any) {
+    }
+    
+    @IBAction func vendorMenuOptionSelected(_ sender: Any) {
+    }
+    
+    @IBAction func generatePassButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func populateDataButtonTapped(_ sender: Any) {
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Helper Methods
 
 }
 
